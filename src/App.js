9cd5db2c8ddb0cb8dbb6fa3aa43f8dc93bfe8c68
danielnolasco1992 <danielnolasco07@gmail.com>
2023-06-13 11:41:14 -0400

    btn64

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
+import Axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+
+
+
+const imageHandler =  (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0])
+    const input = e.target.files[0];
+    reader.onload = async () => {      
+      const base64 = reader.result.split(',')[1];
+      console.log(base64);  
+      guardar (base64)
+      
+  };
+};
+
+
+const guardar  = async (base64) => {
+  const params = { p_nombre_doc: 'PRUEBA1',
+  p_documento:base64  }
+const response = await Axios.post("http://dev-segurospiramide.com/asg-api/dbo/intranet/sp_guardar_doc",params)
+
+console.log(4,response.data)
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>PRUEBA EXCEL</h1>
+<form action="#">
+              <input type="file" accept="" name="image-upload" id="input" onChange={imageHandler} />
+              <label htmlFor="input">
+            
+              </label>
+            </form>
     </div>
   );
 }
